1e5e9fd4fe5a600b78723ec2071ad4f4d5bdda6d
Vasu Parashar <136962787+whotfvasu@users.noreply.github.com>
2025-11-26 13:48:47 +0530

    Removed Toolbox for eraser tool

--- src/components/Toolbox/Toolbox.jsx
+++ src/components/Toolbox/Toolbox.jsx
@@ -19,63 +19,69 @@ const Toolbox = () => {
   const fillColor = toolboxState[activeTool]?.fill;
   const size = toolboxState[activeTool]?.size;
   return (
-    <div className={classes.container}>
-      {STROKE_TOOL_TYPES.includes(activeTool) && (
-        <div className={classes.selectOptionContainer}>
-          <div className={classes.toolboxLabel}>Stroke Color</div>
-          <div className={classes.colorsContainer}>
-            {Object.keys(COLORS).map((c) => {
-              return (
-                <div
-                  key={c}
-                  className={cx(classes.colorBox, {
-                    [classes.activeColorBox]: strokeColor === COLORS[c],
-                  })}
-                  style={{ backgroundColor: COLORS[c] }}
-                  onClick={() => changeStroke(activeTool, COLORS[c])}
-                ></div>
-              );
-            })}
-          </div>
-        </div>
-      )}
+    <>
+      {activeTool !== TOOL_ITEMS.ERASER && (
+        <div className={classes.container}>
+          {STROKE_TOOL_TYPES.includes(activeTool) && (
+            <div className={classes.selectOptionContainer}>
+              <div className={classes.toolboxLabel}>Stroke Color</div>
+              <div className={classes.colorsContainer}>
+                {Object.keys(COLORS).map((c) => {
+                  return (
+                    <div
+                      key={c}
+                      className={cx(classes.colorBox, {
+                        [classes.activeColorBox]: strokeColor === COLORS[c],
+                      })}
+                      style={{ backgroundColor: COLORS[c] }}
+                      onClick={() => changeStroke(activeTool, COLORS[c])}
+                    ></div>
+                  );
+                })}
+              </div>
+            </div>
+          )}
 
-      {FILL_TOOL_TYPES.includes(activeTool) && (
-        <div className={classes.selectOptionContainer}>
-          <div className={classes.toolboxLabel}>Fill Color</div>
-          <div className={classes.colorsContainer}>
-            {Object.keys(COLORS).map((c) => {
-              return (
-                <div
-                  key={c}
-                  className={cx(classes.colorBox, {
-                    [classes.activeColorBox]: fillColor === COLORS[c],
-                  })}
-                  style={{ backgroundColor: COLORS[c] }}
-                  onClick={() => changeFill(activeTool, COLORS[c])}
-                ></div>
-              );
-            })}
-          </div>
-        </div>
-      )}
+          {FILL_TOOL_TYPES.includes(activeTool) && (
+            <div className={classes.selectOptionContainer}>
+              <div className={classes.toolboxLabel}>Fill Color</div>
+              <div className={classes.colorsContainer}>
+                {Object.keys(COLORS).map((c) => {
+                  return (
+                    <div
+                      key={c}
+                      className={cx(classes.colorBox, {
+                        [classes.activeColorBox]: fillColor === COLORS[c],
+                      })}
+                      style={{ backgroundColor: COLORS[c] }}
+                      onClick={() => changeFill(activeTool, COLORS[c])}
+                    ></div>
+                  );
+                })}
+              </div>
+            </div>
+          )}
 
-      {SIZE_TOOL_TYPES.includes(activeTool) && (
-        <div className={classes.selectOptionContainer}>
-          <div className={classes.toolboxLabel}>
-            {activeTool === TOOL_ITEMS.TEXT ? "Font Size" : "Brush Size"}
-          </div>
-          <input
-            type="range"
-            min={activeTool === TOOL_ITEMS.TEXT ? 12 : 1}
-            max={activeTool === TOOL_ITEMS.TEXT ? 64 : 10}
-            step={1}
-            value={size}
-            onChange={(e) => changeSize(activeTool, parseInt(e.target.value))}
-          />
+          {SIZE_TOOL_TYPES.includes(activeTool) && (
+            <div className={classes.selectOptionContainer}>
+              <div className={classes.toolboxLabel}>
+                {activeTool === TOOL_ITEMS.TEXT ? "Font Size" : "Brush Size"}
+              </div>
+              <input
+                type="range"
+                min={activeTool === TOOL_ITEMS.TEXT ? 12 : 1}
+                max={activeTool === TOOL_ITEMS.TEXT ? 64 : 10}
+                step={1}
+                value={size}
+                onChange={(e) =>
+                  changeSize(activeTool, parseInt(e.target.value))
+                }
+              />
+            </div>
+          )}
         </div>
       )}
-    </div>
+    </>
   );
 };
 
